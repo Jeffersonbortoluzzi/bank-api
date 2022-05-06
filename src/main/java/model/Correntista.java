5fe1567aef8455d49861b61df404b5af29310651
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2022-05-06 16:38:32 -0300

    Mapeando as entidades com JPA

---
+++ src/main/java/model/Correntista.java
@@ -0,0 +1,55 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tab_correntista")
+
+public class Correntista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 7)
+    private String cpf;
+
+    @Column(length = 60)
+    private String nome;
+
+    //VIDEO 41MINUTOS VER DPS PRA ANOTAR
+    @Embedded
+    private Conta conta;
+
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/model/MovimenTipo.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum MovimenTipo {
+    RECEITA,
+    DESPESA
+}
